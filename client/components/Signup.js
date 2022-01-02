c8de5b4774a745a12872d789b56a7f15b624e691
Liz Yoder <EAHYoder@gmail.com>
2022-01-02 14:16:18 -0500

    refactored signup component to be with react hooks

---
+++ client/components/Signup.js
@@ -0,0 +1,40 @@
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { authenticate } from "../store";
+
+export const Signup = () => {
+  const error = useSelector((state) => state.auth.error);
+
+  const dispatch = useDispatch();
+
+  const handleSubmit = (evt) => {
+    evt.preventDefault();
+    const formName = evt.target.name;
+    const username = evt.target.username.value;
+    const password = evt.target.password.value;
+    dispatch(authenticate(username, password, formName));
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit} name="signup">
+        <div>
+          <label htmlFor="username">
+            <small>Username</small>
+          </label>
+          <input name="username" type="text" />
+        </div>
+        <div>
+          <label htmlFor="password">
+            <small>Password</small>
+          </label>
+          <input name="password" type="password" />
+        </div>
+        <div>
+          <button type="submit">Sign Up</button>
+        </div>
+        {error && error.response && <div> {error.response.data} </div>}
+      </form>
+    </div>
+  );
+};
