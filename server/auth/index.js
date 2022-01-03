c6b906edfa777b4618339452c9fce80d607bd0a1
Liz Yoder <EAHYoder@gmail.com>
2022-01-03 16:21:36 -0500

    fixed issue where the sign up button did not perform full authentication

--- server/auth/index.js
+++ server/auth/index.js
@@ -17,8 +17,9 @@ router.post("/login", async (req, res, next) => {
 //this posts a new user in the database and then generates a token for them.
 router.post("/signup", async (req, res, next) => {
   try {
-    const user = await User.create(req.body);
-    res.send({ token: await user.generateToken() });
+    await User.create(req.body);
+    const token = await User.authenticate(req.body);
+    res.send({ token });
   } catch (err) {
     if (err.name === "SequelizeUniqueConstraintError") {
       res.status(401).send("User already exists");
