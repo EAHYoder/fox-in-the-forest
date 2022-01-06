d139f5b17032178bd8eee16ad305ff5fcaad64f9
Liz Yoder <EAHYoder@gmail.com>
2022-01-06 10:10:13 -0500

    fixed issue where fox didn't move to the left.  made it so winner player leads the next trick.  cleared cards to start next trick

--- client/components/Board.js
+++ client/components/Board.js
@@ -2,6 +2,7 @@ import React, { useEffect, useRef } from "react";
 import Space from "./Space";
 import { fetchSpaces, setSpaces } from "../store/spaces";
 import { goUpdatePlayers } from "../store/players";
+import { setPlayer0Card, setPlayer1Card } from "../store/playedCards";
 import { useSelector, useDispatch } from "react-redux";
 import anime from "animejs/lib/anime.es.js";
 
@@ -42,8 +43,9 @@ const Board = (props) => {
 
   const determineWinner = () => {
     let winner = null;
+
     let sameSuit = player0Card.suit === player1Card.suit;
-    console.log("the same suit was played:", sameSuit);
+
     if (sameSuit) {
       //if the cards are the same suit the card with the highest number wins
       winner = player0Card.number > player1Card.number ? "player0" : "player1";
@@ -53,10 +55,6 @@ const Board = (props) => {
 
       let trumpPlayed =
         player0Card.suit === decree.suit || player1Card.suit === decree.suit;
-      console.log(
-        "different suits were played and one was the trump:",
-        trumpPlayed
-      );
 
       if (trumpPlayed) {
         winner = player0Card.suit === decree.suit ? "player0" : "player1";
@@ -75,12 +73,13 @@ const Board = (props) => {
   useEffect(() => {
     if (trickEnded) {
       // evaluate who won the trick
-      const winner = determineWinner;
+      const winner = determineWinner();
 
       //if player0 wins then movement is negative.  if player1 wins then movement is positive.
       let movementAmount = player0Card.movement + player1Card.movement;
       let translateFox =
         winner === "player0" ? -100 * movementAmount : 100 * movementAmount;
+      //changing the direction in which the fox should move doesn't seem to be working.
 
       const happensAfterAnime = () => {
         //dispatch to store information about the new fox location on the board.
@@ -100,10 +99,24 @@ const Board = (props) => {
         newSpaces.splice(newFoxSpaceIdx, 1, newFoxSpace);
         dispatch(setSpaces(newSpaces));
 
-        //dipatch to store and emit information about switching leading player.
-        player0.isLeading = !player0.isLeading;
-        player1.isLeading = !player1.isLeading;
+        //dipatch to store and emit information about who should lead the next trick (whoever won this trick)
+        if (winner === "player0") {
+          player0.isLeading = true;
+          player0.isActive = true;
+          player1.isLeading = false;
+          player1.isActive = false;
+        }
+        if (winner === "player1") {
+          player0.isLeading = false;
+          player0.isActive = false;
+          player1.isLeading = true;
+          player1.isActive = true;
+        }
         dispatch(goUpdatePlayers([player0, player1]));
+
+        //clear the played cards so the next trick can be played.
+        dispatch(setPlayer0Card({}));
+        dispatch(setPlayer1Card({}));
       };
 
       //animate fox movement.
